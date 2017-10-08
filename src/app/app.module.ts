c4401d3f4337fd2bbbe285bd2b0df6e3ce90515b
Patryk Bielecki <p.patrykbielecki@gmail.com>
2017-10-08 16:07:10 +0200

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EffectsComponent } from './effects/effects.component';
+import { PanelComponent } from './panel/panel.component';
+import { ReverbComponent } from './reverb/reverb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EffectsComponent,
+    PanelComponent,
+    ReverbComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
